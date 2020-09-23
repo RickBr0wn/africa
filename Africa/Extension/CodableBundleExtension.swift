222d5d7e7550df65af81a242eec6197ea597a1e0
Rick Brown <ricky.brown.00@gmail.com>
2020-09-23 17:38:10 +0100

    adds dynamic list using generics and json data

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-  func decode(_ file: String) -> [CoverImage] {
+  func decode<T: Codable>(_ file: String) -> T {
     // locate the json file in the Bundle
     guard let url = self.url(forResource: file, withExtension: nil) else {
       fatalError("Failed to locate the file in Bundle")
@@ -23,7 +23,7 @@ extension Bundle {
     let decoder = JSONDecoder()
     
     // create a property for the decoded data
-    guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+    guard let loaded = try? decoder.decode(T.self, from: data) else {
       fatalError("Failed to decode file from Bundle")
     }
     
---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Rick Brown on 23/09/2020.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+  let id: String
+  let name: String
+  let headline: String
+  let description: String
+  let link: String
+  let image: String
+  let gallery: [String]
+  let fact: [String]
+}
