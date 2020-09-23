f4e4964afea5be907c4eb32f183340e297114077
Rick Brown <ricky.brown.00@gmail.com>
2020-09-23 16:53:21 +0100

    adds cover images and parses json data

---
+++ Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,33 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by Rick Brown on 23/09/2020.
+//
+
+import Foundation
+
+extension Bundle {
+  func decode(_ file: String) -> [CoverImage] {
+    // locate the json file in the Bundle
+    guard let url = self.url(forResource: file, withExtension: nil) else {
+      fatalError("Failed to locate the file in Bundle")
+    }
+    
+    // create a property for the data
+    guard let data = try? Data(contentsOf: url) else {
+      fatalError("Failed to load Bundle")
+    }
+    
+    // create a decoder
+    let decoder = JSONDecoder()
+    
+    // create a property for the decoded data
+    guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+      fatalError("Failed to decode file from Bundle")
+    }
+    
+    // return the ready to use data
+    return loaded
+  }
+}
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Rick Brown on 23/09/2020.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
