d94d9e776dc826db2f31baf619994bbed42e4d02
Rick Brown <ricky.brown.00@gmail.com>
2020-09-23 16:14:32 +0100

    adds assets and navigation

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Rick Brown on 23/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+  var body: some Scene {
+    WindowGroup {
+      MainView()
+    }
+  }
+}
---
+++ Africa/App/Gallery.swift
@@ -0,0 +1,20 @@
+//
+//  Gallery.swift
+//  Africa
+//
+//  Created by Rick Brown on 23/09/2020.
+//
+
+import SwiftUI
+
+struct Gallery: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+struct Gallery_Previews: PreviewProvider {
+    static var previews: some View {
+        Gallery()
+    }
+}
